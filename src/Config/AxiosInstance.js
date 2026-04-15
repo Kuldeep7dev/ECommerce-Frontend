e7bf98ca1bedc5a2f820024965818e5723504a25
Kuldeep7devi <kushallaxkar9@gmail.com>
2026-04-15 15:23:20 +0530

    Change baseURl

--- src/Config/AxiosInstance.js
+++ src/Config/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9090",
+    baseURL: "https://bravimaserver.vercel.app/",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json"
